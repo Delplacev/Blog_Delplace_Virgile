86f5ec46a06775472713e147a128ebd54f7e5c92
Headoverz <djvivi59@gmail.com>
2016-02-23 14:07:35 +0100

    erreur 1

--- TP_blog_DelplaceVirgile/assets/protected/connectBDD.php
+++ TP_blog_DelplaceVirgile/assets/protected/connectBDD.php
@@ -1,20 +1,16 @@
 <?php
-
 $server_name = "localhost";
 $user = "root";
-$mdp = "root";
-
+$mdp = "";
 //Connexion
 $link = mysql_connect($server_name, $user, $mdp );
-
 //Si connection fail
 if (!$link) {
 	die('Connexion impossible : ' . mysql_error());
 }
 		
-
 //select de la base 
-$db_selected = mysql_select_db('BDD_TP_LANNOY', $link);
+$db_selected = mysql_select_db('bdd_blog_delplace_virgile', $link);
 if (!$db_selected) {
 	die ('Impossible de sélectionner la base de données : ' . mysql_error());
 }
