dba0ff76162fc2156414ea447129056a1bc4dda5
Headoverz <djvivi59@gmail.com>
2016-02-23 14:28:10 +0100

    erreur 2

--- TP_blog_DelplaceVirgile/index.php
+++ TP_blog_DelplaceVirgile/index.php
@@ -34,13 +34,16 @@ include('assets/protected/connectBDD.php');
 
                 $result = mysql_query($query);
 
+                 if(mysql_num_rows($result) == 0 ) {
+                    echo "<p>Aucune résultat ne correspond aà votre rcherche";
+                 }
                 while ($ligne = mysql_fetch_assoc($result)) {
                     // Affichage des article
 
                     echo '<center><h3><a href="show.php?idArticle='.$ligne[id].'"">' . $ligne[titre] . '</a></h3><br>';
                     echo '<img src="assets/include/vignettejpg.php?imgSrc=' . $ligne[nomImage] . '" ><br><br>';
                     echo $ligne[description] . '<br> <br><p class="rigth"> date : ' . $ligne[date] . '</p>';
-                    if ($connect == true) {
+                    if ($connect == true) {var_dump(mysql_num_rows($result));
                         echo '<a href="editerArticle.php?idArticle=' . $ligne[id] . '"><input type="button" class="btn-primary" value="Editer"></a>';
                         echo '<a href="assets/include/action.php?action=deleteArticle&idArticle=' . $ligne[id] . '"><input type="button" class="btn-primary" id="btnSuppr" value="Suprimer "></a></br></center><hr>';
                     } else {
