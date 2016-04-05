10ce5a09d42e5313e1476b859ea2e9e07c8ae59b
Headoverz <djvivi59@gmail.com>
2016-04-05 16:53:35 +0200

    Map Erreur commit

--- .Trash/TP_blog_DelplaceVirgile 16.26.40/assets/include/setCookie.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Cookie qui compte le nombre de chargement
-if(isset($_COOKIE["nbChargement"])){
-	setcookie("nbChargement",$_COOKIE["nbChargement"] +1 ,time() + 365*24*3600);
-} else{
-	setcookie("nbChargement","1" ,time() + 365*24*3600);
-}
